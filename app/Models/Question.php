47e937eac0cc3ea9b7bc9d1ae0f2f7cf5cd841d2
Albin Varghese <albin8285@gmail.com>
2023-04-22 00:12:58 +0530

    fix: add fillable parameters for Question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'body',
+    ];
 }
