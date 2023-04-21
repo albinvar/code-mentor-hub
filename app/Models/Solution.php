cdf2c319a90f905495d47e3be3d19ebb56a121da
Albin Varghese <albin8285@gmail.com>
2023-04-22 00:42:29 +0530

    chore: create basic relationships between votes and solutions

--- app/Models/Solution.php
+++ app/Models/Solution.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Solution extends Model
 {
@@ -15,13 +17,20 @@ class Solution extends Model
         'body',
     ];
 
-    public function question()
+    public function question(): BelongsTo
     {
         return $this->belongsTo(Question::class);
     }
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
+
+    public function votes(): HasMany
+    {
+        return $this->hasMany(Vote::class);
+    }
+
+
 }
--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -14,4 +14,14 @@ class Vote extends Model
         'user_id',
         'solution_id',
     ];
+
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+
+    public function solution()
+    {
+        $this->belongsTo(Solution::class);
+    }
 }
