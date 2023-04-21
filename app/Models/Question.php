6f0f8db8beed4ce09412cbcd3203c7eb91b7febc
Albin Varghese <albin8285@gmail.com>
2023-04-22 00:26:33 +0530

    chore: create one-to-many relation between user and questions model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Question extends Model
 {
@@ -15,4 +16,9 @@ class Question extends Model
         'slug',
         'body',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
