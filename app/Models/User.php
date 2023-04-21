e5798ae8a76c4040fc97e3445af55a88cdeeab88
Albin Varghese <albin8285@gmail.com>
2023-04-22 00:47:29 +0530

    chore: add relation between user and vote models

--- app/Models/User.php
+++ app/Models/User.php
@@ -77,4 +77,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Solution::class);
     }
+
+    public function votes(): HasMany
+    {
+        return $this->hasMany(Vote::class);
+    }
 }
