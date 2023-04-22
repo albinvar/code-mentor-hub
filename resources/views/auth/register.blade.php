3fdebc836c6cec127fb01b77ce657dd16c22ff4a
Albin Varghese <albin8285@gmail.com>
2023-04-22 19:06:31 +0530

    feat: ask users interests at the time of registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,7 +29,43 @@
                 <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+            <div class="mt-4" x-data="{ tags: [], newTag: '' }" x-init="() => {
+    @if (old('tags'))
+        tags.push(...JSON.parse('{{ json_encode(old("tags")) }}'))
+    @endif
+}">
+                <x-label for="tags" value="{{ __('Your Interests') }}" />
+                <div class="my-3 flex flex-wrap items-center gap-2" x-cloak>
+                    <template x-for="(tag, index) in tags" :key="index">
+                        <div class="px-2 py-1 bg-gray-200 rounded-lg dark:bg-gray-700 transition-opacity duration-900 opacity-100 hover:opacity-75">
+                            <span x-text="tag"></span>
+                            <button type="button" class="ml-2 text-red-500 hover:text-red-700 focus:outline-none" @click.prevent="tags.splice(index, 1)">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                          d="M11.414 10l4.293-4.293a1 1 0 10-1.414-1.414L10 8.586 5.707 4.293a1 1 0 10-1.414 1.414L8.586 10l-4.293 4.293a1 1 0 001.414 1.414L10 11.414l4.293 4.293a1 1 0 001.414-1.414L11.414 10z"
+                                          clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                        </div>
+                    </template>
+
+                    <input type="text" id="tags" class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" x-model="newTag" x-on:keydown.enter.prevent="
+            if (newTag.trim()) {
+                let tagsToAdd = newTag.split(/[ ,]+/);
+                tagsToAdd.forEach(tag => {
+                    if (tag.trim() && !tags.includes(tag.trim())) {
+                        tags.push(tag.trim());
+                    }
+                });
+                newTag = '';
+            }" placeholder="Add tags (comma, space, or enter separated)">
+                </div>
+                <input type="hidden" name="tags" :value="JSON.stringify(tags)">
+            </div>
+
+
+
+        @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
                 <div class="mt-4">
                     <x-label for="terms">
                         <div class="flex items-center">
