60326efb0607e52450c68c1c488c227ace0cdf51
Albin Varghese <albin8285@gmail.com>
2023-04-24 22:42:07 +0530

    chore: create a new model OneToOneSession with all resources

---
+++ app/Http/Controllers/OneToOneSessionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OneToOneSession;
+use App\Http\Requests\StoreOneToOneSessionRequest;
+use App\Http\Requests\UpdateOneToOneSessionRequest;
+
+class OneToOneSessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOneToOneSessionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OneToOneSession $oneToOneSession)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OneToOneSession $oneToOneSession)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOneToOneSessionRequest $request, OneToOneSession $oneToOneSession)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OneToOneSession $oneToOneSession)
+    {
+        //
+    }
+}
